efcb4a9f4923c58ff25a30f5273bd57e6c90fedb
Toni Birrer <github@dexter.cc>
2021-08-03 14:16:23 +0200

    working fakenews dataloader plugin

---
+++ data_loader/8_fakenews/load.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+CLUSTER="https://..."
+TOKEN="..."
+PROJECT_ID="..."
+
+squirro_data_load -v \
+    --cluster $CLUSTER \
+    --token $TOKEN \
+    --project-id $PROJECT_ID \
+    --source-script 'fakenews_plugin.py' \
+    --source-name "Fake News" \
+    --source-batch-size 100 \
+    --batch-size 100 \
+    --endpoint "https://fakenews.squirro.com" \
+    --section "sport" "finance" \
+    --map-title 'headline' \
+    --map-body 'body' \
+    --map-id 'id' \
+    --map-url 'article_uri' \
+    --map-created-at 'date' \
+    --facets-file 'facets.json'
